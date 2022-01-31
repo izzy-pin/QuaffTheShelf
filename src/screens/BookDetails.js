390312487533e73bae5e71bfb0d41dcd1bb2ba8b
mrtim2117 <tim.staton@outlook.com>
2022-01-31 16:32:37 +0000

    displaying full book details from database

--- src/screens/BookDetails.js
+++ src/screens/BookDetails.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, Image, StyleSheet } from "react-native";
 import { readBookDetails } from "../utils/firebase-funcs";
+import { getAuth } from "firebase/auth";
+
 import defaultCover from "../assets/defaultCover.png";
 const BookDetails = ({ route }) => {
   const { isbn } = route.params;
   const [book, setBook] = useState({});
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const email = user.email;
 
   useEffect(() => {
     readBookDetails(isbn)
@@ -17,29 +23,61 @@ const BookDetails = ({ route }) => {
       });
   }, []);
   return (
-    <View>
-      <Text>{book.bookTitle}</Text>
-      <Text>
-        {book.bookSubTitle === "No Subtitle found" ? null : book.bookSubTitle}
-      </Text>
-      <Text>{book.bookAuthor}</Text>
-      <Image
-        style={styles.bookImage}
-        source={
-          book.bookCover === "No image found"
-            ? defaultCover
-            : { uri: book.bookCover }
-        }
-      />
+    <View style={styles.container}>
+      <View style={styles.book}>
+        <View style={styles.bookText}>
+          <Text style={styles.bookTitle}>{book.bookTitle}</Text>
+          <Text>
+            {book.bookSubTitle === "No Subtitle found"
+              ? null
+              : book.bookSubTitle}
+          </Text>
+          <Text>{book.bookAuthor}</Text>
+        </View>
+
+        <Image
+          style={styles.bookImage}
+          source={
+            book.bookCover === "No image found"
+              ? defaultCover
+              : { uri: book.bookCover }
+          }
+        />
+      </View>
+      <View>
+        {Object.keys(book).includes("drinkPairings") &&
+        Object.keys(book.drinkPairings).includes(email) ? (
+          <Text style={styles.drink}>
+            Drink: {book.drinkPairings[email].drink}
+          </Text>
+        ) : null}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  book: {
+    margin: 10,
+  },
   bookImage: {
     height: 118,
     resizeMode: "contain",
     width: 90,
+  },
+  bookText: {
+    marginBottom: 10,
+  },
+  bookTitle: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  container: {
+    alignItems: "center",
+    flex: 1,
+  },
+  drink: {
+    fontSize: 20,
   },
 });
 
